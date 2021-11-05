104c2f0770f2e914c7a7e2ce383a4d7eb08a5c8d
João Victor Oliveira Santos <jvos@cin.ufpe.br>
2021-11-05 00:03:13 -0300

    init project

---
+++ src/main/java/me/jvos/springcrudeproto/SpringcrudeProtoApplication.java
@@ -0,0 +1,13 @@
+package me.jvos.springcrudeproto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcrudeProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcrudeProtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jvos/springcrudeproto/SpringcrudeProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jvos.springcrudeproto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcrudeProtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
